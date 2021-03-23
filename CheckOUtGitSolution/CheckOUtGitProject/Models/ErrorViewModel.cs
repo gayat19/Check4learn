c731476ec38c6fbd8debbb090436ba959c5b0413
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-03-23 10:46:24 +0530

    My Dotnet Project

---
+++ CheckOUtGitSolution/CheckOUtGitProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CheckOUtGitProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
